c987ba24d2062e054a5b9c69ad5d8468156172fe
sewunetwolde <nekodmos@gmail.com>
2023-03-07 15:33:26 +0300

    nnn

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,13 +12,13 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-auth = None
-AUTH_TYPE = getenv('AUTH_TYPE')
 
-if AUTH_TYPE == "auth":
+auth = None
+
+if os.getenv("AUTH_TYPE") == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
-elif AUTH_TYPE == "basic_auth":
+elif os.getenv("AUTH_TYPE") == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
@@ -31,48 +31,33 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def unauthorized(error) -> str:
-    """Handle a unauthorized access
-        Args:
-            error: Error catch
-        Return:
-            Info of the error
+def unauthorized_error(error) -> str:
+    """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden(error) -> str:
-    """Handle a forbidden resource
-        Args:
-            error: Error catch
-        Return:
-            Info of the error
+def forbidden_error(error) -> str:
+    """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
 def before_request() -> str:
-    """Execute before each request
-        Return:
-            String or nothing
+    """ Filter for request
     """
-    if auth is None:
-        return
-
-    expath = ['/api/v1/status/',
-              '/api/v1/unauthorized/',
-              '/api/v1/forbidden/']
-
-    if not (auth.require_auth(request.path, expath)):
-        return
-
-    if (auth.authorization_header(request)) is None:
-        abort(401)
-
-    if (auth.current_user(request)) is None:
-        abort(403)
+    request_path_list = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/']
+    if auth:
+        if auth.require_auth(request.path, request_path_list):
+            if auth.authorization_header(request) is None:
+                abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,19 +26,17 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
-def test_forbidden() -> str:
-    """ GET /api/v1/forbidden
-    Return:
-      - Raise error
-    """
-    return abort(403)
-
-
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def test_unathourized() -> str:
+def unauthorized() -> str:
     """ GET /api/v1/unauthorized
-    Return:
-      - Raise error
+    Return - raises 401 error
     """
-    return abort(403)
+    abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return - raises 403 error
+    """
+    abort(403)
